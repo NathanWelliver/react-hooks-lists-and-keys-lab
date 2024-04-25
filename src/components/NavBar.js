887c895780f0941de50dce559b5b4df3e0f1d900
Nathan Welliver <nwelliver.564@gmail.com>
2024-04-25 13:27:36 -0400

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const mappedLinks = links.map((link, index) => {
+    return <a key={index} href={`#${link}`} >{link}</a>
+  })
+
+  return <nav>{mappedLinks}</nav>;
 }
 
 export default NavBar;
